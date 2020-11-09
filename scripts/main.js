c283b41967b43431762edd0043046b05650ed23d
Veljko <veljkos82@gmail.com>
2020-11-09 19:08:12 +0100

    Redone portfolio as per suggestions.

--- scripts/main.js
+++ scripts/main.js
@@ -88,3 +88,32 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.open(map);
 
 }
+
+$( document ).ready(function() {
+
+  // Carousel
+
+  $(".carousel").carousel({
+      interval: false,
+      pause: true
+  });
+
+  $( ".carousel .carousel-inner" ).swipe( {
+  swipeLeft: function ( event, direction, distance, duration, fingerCount ) {
+      this.parent( ).carousel( 'next' );
+  },
+  swipeRight: function ( ) {
+      this.parent( ).carousel( 'prev' );
+  },
+  threshold: 0,
+  tap: function(event, target) {
+      window.location = $(this).find('.carousel-item.active a').attr('href');
+  },
+  excludedElements:"label, button, input, select, textarea, .noSwipe"
+  } );
+
+  $('.carousel .carousel-inner').on('dragstart', 'a', function () {
+      return false;
+  });  
+
+});
